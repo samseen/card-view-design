34d216e1c3225c13c0b5197b4b3d2cf4395d3df5
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-25 09:29:33 +0100

    Clicking each card displays a toast notification.

--- app/src/main/java/com/example/cardviewdesign/MainActivity.java
+++ app/src/main/java/com/example/cardviewdesign/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.cardviewdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView cardHome;
+    CardView cardChat;
+    CardView cardProfile;
+    CardView cardWidget;
+    CardView cardSettings;
+    CardView cardLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardHome = findViewById(R.id.cardHome);
+        cardChat = findViewById(R.id.cardChat);
+        cardProfile = findViewById(R.id.cardProfile);
+        cardWidget = findViewById(R.id.cardWidget);
+        cardSettings = findViewById(R.id.cardSettings);
+        cardLogout = findViewById(R.id.cardLogout);
+
+        cardHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Home Clicked");
+            }
+        });
+
+        cardChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Card Clicked");
+            }
+        });
+
+        cardProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Profile Clicked");
+            }
+        });
+
+        cardWidget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Widget Clicked");
+            }
+        });
+
+        cardSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Settings Clicked");
+            }
+        });
+
+        cardLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("Logout clicked");
+            }
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
